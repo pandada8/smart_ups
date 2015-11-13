4952ab3a87fa62b66202e0053c89f485f3f40937
Pandada8 <pandada8@gmail.com>
2015-11-13 19:09:59 +0800

    add scan and list function

---
+++ parse.py
@@ -0,0 +1,33 @@
+
+import re
+from pyparsing import *
+
+__all__ = ["parse", "dump"]
+def elemAction(i, t, ret):
+    if len(ret) == 1:
+        ret.append(True)
+    return ret
+
+def obj_like(i, t, ret):
+    key = ret[::2]
+    value = ret[1::2]
+    return dict(zip(key, value))
+
+obj = Forward()
+elem_member = Word(alphanums + "@.\\ _").leaveWhitespace()
+elem = elem_member + Optional(Suppress(":") + (elem_member | obj)) + Suppress(";")
+elem.parseAction = [elemAction]
+obj << Suppress("{") + ZeroOrMore(elem) + Suppress("}")
+obj.parseAction = [obj_like]
+
+def parse(text):
+    if isinstance(text, bytes) or isinstance(text, bytearray):
+        text = text.decode("ascii")
+    return obj.parseString(text).asList()[0]
+
+def dump(o):
+    # o should be a dict
+    if isinstance(o, dict):
+        return "{" + ''.join([str(k) + ":" + dump(v) + ";" for k, v in o.items()]) + "}"
+    else:
+        return str(o)
--- upsctl.py
+++ upsctl.py
@@ -4,9 +4,9 @@
 Usage:
 upsctl.py scan
 upsctl.py list
-upsctl.py stop <machine> <-all>
-upsctl.py status <machine> <-all>
-upsctl.py start <machine> <-all>
+upsctl.py stop <machine>... <-all>
+upsctl.py status <machine>... <-all>
+upsctl.py start <machine>... <-all>
 
 Options:
     -v            be Verbose
@@ -15,21 +15,20 @@ Options:
 
 import socket
 import docopt
+import threading
 import logging
 import os
 import json
+import time
+from parse import parse, dump
 
-doc = None
+opt = None
 configPath = "~/.config/smart_ups.json" if os.name == "nt" else "~/smart_ups.json"
 configPath = os.path.expanduser(configPath)
 config = {
     "machines": []
 }
 log = logging.getLogger()
-
-
-def parse(byte):
-    pass
 
 
 def load_config():
@@ -48,16 +47,66 @@ def load_config():
 
 def save_config():
     with open(configPath, "w") as fp:
-        json.dump(config, fp)
+        json.dump(config, fp, indent=4, ensure_ascii=False)
         log.debug("Save Config to %s", configPath)
 
+
+def get_machines():
+    global config, opt
+    if opt['--all']:
+        return config["machines"]
+    else:
+        ret = []
+        machines = {m['ip']: m for m in config['machines']}
+        for i in opt["<machine>"]:
+            if i in machines:
+                ret.append(machines)
+            else:
+                print("Can't find {}".format(i))
+        return ret
 
 def _stop():
     raise NotImplementedError
 
 
 def _scan():
-    raise NotImplementedError
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(b'headcall.device.lookup {type:UPS;}', ("255.255.255.255", 9600))
+    t = time.time()
+    sock.settimeout(1)
+    result = {}
+    def finding(sock):
+        sock.settimeout(10)
+        start = time.time()
+        while time.time() - start < 10:
+            try:
+                payload, ip = sock.recvfrom(1024)
+                ip, _ = ip
+                _, payload = payload.split(b" ", 1)
+                uid = parse(payload)["uid"]
+                result[ip] = {
+                    "ip": ip,
+                    "uid": uid
+                }
+            except socket.timeout:
+                break
+
+    # Start a new Thread to run the listener
+    t = threading.Thread(target=finding, args=(sock, ))
+    t.start()
+    while t.is_alive():
+        font = r"-\|/"[int(time.time()*10)%4]
+        print("\rScanning {}".format(font), end="")
+        time.sleep(0.1)
+    config["machines"] = result
+    print("\rFinish scanning")
+    print("Found {} ups".format(len(result)))
+
+
+
+
 
 
 def _start():
@@ -69,16 +118,25 @@ def _status():
 
 
 def _list():
-    print("Found {} machins".format(len(config["machines"])))
-    for i in config["machines"]:
-        print(i['ip'])
+    nums = len(config["machines"])
+    if nums == 0:
+        print("No Machine")
+        return
+    print("Found {} machines\n".format(nums))
+    print("IP\t\t\tMachine UID")
+    for i, j in config["machines"].items():
+        print("{ip}\t\t{uid}".format(**j))
 
 
 def main():
-    global doc
-    doc = docopt.docopt(__doc__)
+    global opt
+    opt = docopt.docopt(__doc__)
+    if "-v" in opt:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
     for action in ["scan", "list", "stop", "status", "start"]:
-        if doc[action]:
+        if opt[action]:
             load_config()
             globals()["_" + action]()
             save_config()
